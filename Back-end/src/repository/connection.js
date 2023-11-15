2a05c25e985899c7be714fd04030863b199a6774
Alex Sousa <105399426+Alekey2@users.noreply.github.com>
2023-11-15 14:01:49 -0300

    Alterado

--- Back-end/src/repository/connection.js
+++ Back-end/src/repository/connection.js
@@ -1,10 +1,10 @@
 import mysql2 from 'mysql2/promise';
 
 const conectar = await mysql2.createConnection({
-    host: process.env.MYSQL_HOST,
-    database: process.env.MYSQL_DB,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PWD 
+    host: '4.228.66.214',
+    database: 'tocaraul',
+    user: 'admin',
+    password: '@dm!n', 
 })
 
 console.log('Conexão com o BD realizada com sucesso !!');
